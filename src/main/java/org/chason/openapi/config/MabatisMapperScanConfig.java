f5b42dba4beb5707e3f8cc7a261326c5c784e3f1
cangqiong <cang19910623@163.com>
2018-03-10 16:55:02 +0800

    修改mybatis配置

--- src/main/java/org/chason/openapi/config/MabatisMapperScanConfig.java
+++ src/main/java/org/chason/openapi/config/MabatisMapperScanConfig.java
@@ -1,6 +1,5 @@
 package org.chason.openapi.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
